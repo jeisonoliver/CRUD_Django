6b150ae3744c26ca603545964c7dad2527f8df8b
jeisonoliver <jeisonjs42@gmail.com>
2022-04-30 16:05:41 -0300

    Aprendendo Django

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class product(models.Model):
+class Product(models.Model):
     description = models.CharField(max_length = 100)
     price = models.DecimalField(max_digits = 9, decimal_places = 2)
     quantity = models.IntegerField()
